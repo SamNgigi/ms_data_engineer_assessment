a96eee513ff5a7d0b2763bd21bb70546431963d2
SamNgigi <sam.t.ngigi@gmail.com>
2020-03-14 17:39:32 +0300

    Initial dashboard implementation done

--- solution/dash_apps/visualization_data.py
+++ solution/dash_apps/visualization_data.py
@@ -3,17 +3,24 @@ import pandas as pd
 from flask_apps.models.Applications import ApplicationsSql
 from flask_apps.models.Outcomes import OutcomesSql
 from flask_apps.models.Classes import ClassesSql
+from flask_apps.models.Admissions import AdmissionsSql
+from flask_apps.models.People import PeopleSql
 
 # Initializations
 applicationsSql = ApplicationsSql()
 outcomesSql = OutcomesSql()
 classesSql = ClassesSql()
+admissionsSql = AdmissionsSql()
+peoples_sql = PeopleSql()
 
 
 # Loading the data
 applications_df = applicationsSql.get_df()
 outcomes_df = outcomesSql.get_df()
 classes_df = classesSql.get_df()
+admissions_df = admissionsSql.get_df()
+peoples_df = peoples_sql.get_df()
+
 
 # Converting date string to datetime object
 applications_df['date'] = pd.to_datetime(applications_df['date'])
@@ -27,16 +34,31 @@ classes_df['end_date'] = pd.to_datetime(classes_df['end_date'])
 # renaming class id to class_id
 classes_df.rename(columns={'id': 'class_id'}, inplace=True)
 
+# Mapping gender to applications, admissions & graduates
+peoples_df.rename(columns={'id': 'person_id'}, inplace=True)
+peoples_df.head()
+
 # *Import this
 class_applications = pd.merge(
     applications_df, classes_df.drop('code', axis=1), how='inner', on='class_id')
+class_applications = pd.merge(class_applications, peoples_df[[
+                              'person_id', 'gender']], how='inner', on='person_id')
+
 
 class_cols = ['class_id', 'class_name', 'end_date']
 
 # *Import this
 graduates = pd.merge(
     outcomes_df, classes_df[class_cols], how='inner', on='class_id')
+graduates = pd.merge(
+    graduates, peoples_df[['person_id', 'gender']], how='inner', on='person_id')
+graduates.head()
 
+# *Import this to dboard
+admitted_students = pd.merge(admissions_df, classes_df[[
+                             'class_id', 'class_name']], how='inner', on='class_id')
+admitted_students = pd.merge(admitted_students, peoples_df[[
+                             'person_id', 'gender']], how='inner', on='person_id')
 
 
 
